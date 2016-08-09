fe704f40f10e689f9863621e56d73604f54ecc91
Jake Baldwin <jake.a.baldwin@gmail.com>
2016-08-09 13:44:12 -0600

    File rearrangement

---
+++ CircularBuffer/CMakeFiles/circular_buffer_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/circular_buffer_test.dir/test/test.c.o"
+  "circular_buffer_test.pdb"
+  "circular_buffer_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/circular_buffer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
