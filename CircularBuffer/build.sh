ad2e0b18bff948d855369da9d85bf9c196726533
Jake Baldwin <jake.a.baldwin@gmail.com>
2016-08-15 13:35:38 -0600

    Improved build

--- CircularBuffer/build.sh
+++ CircularBuffer/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-gcc -o CircularBuffer.o -c -Wall -fpic ./src/CircularBuffer.c -I ./include 
-gcc -shared -o CircularBuffer.so CircularBuffer.o
-gcc -L ./ -Wall -o test.out ./test/test.c -lCircularBuffer -I ./include
+gcc -o CircularBuffer.o -c -Wall -Werror -fpic ./src/CircularBuffer.c -I ./include
+
+gcc -shared -o libCircularBuffer.so CircularBuffer.o
+mv libCircularBuffer.so ./lib
+rm CircularBuffer.o
+
+gcc -L/home/jake/EmbeddedUtilities/CircularBuffer/lib -Wl,-rpath=/home/jake/EmbeddedUtilities/CircularBuffer/lib -Wall -o test.out ./test/test.c -lCircularBuffer -I ./include
+mv test.out ./bin
+
+
