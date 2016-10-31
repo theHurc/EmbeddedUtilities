6c45f4e56fe36c113754f34d59c7dfa0cbfe420b
Jake Baldwin <jake.a.baldwin@gmail.com>
2016-10-31 08:23:03 -0600

    Added framework for scheduler

--- CoOperativeScheduler/include/CoOperativeScheduler/CoOperativeScheduler.h
+++ CoOperativeScheduler/include/CoOperativeScheduler/CoOperativeScheduler.h
@@ -1,4 +1,24 @@
 #ifndef __CO_OPERATIVE_SCHEDULER__
 #define __CO_OPERATIVE_SCHEDULER__
 
+/*
+ * Add a task to the scheduler to run periodically.
+ *
+ * @param task  The function to execute when the task runs.
+ * @param begin_time_ms  Delay time in ms before the first execution of the task
+ * @param repeat_time_ms  Period in ms to run the task
+ */
+void schedulerAddTask( void (*task)(void),
+                       uint32_t begin_time_ms,
+                       uint32_t repeat_time_ms );
+
+//! Start the timer that will schedule the tasks periodically.
+void schedulerStart( void );
+
+//! Execute all the tasks in the task queue. Empties the queue completely.
+void schedulerDispatchTasks( void );
+
+//! Called from the timer interrupt to schedule the tasks
+void scheduleTasks( uint32_t ms);
+
 #endif
---
+++ CoOperativeScheduler/src/CoOperativeScheduler.c
@@ -0,0 +1,20 @@
+#include "CoOperativeScheduler/CoOperativeScheduler.h"
+
+void schedulerAddTask( void (*task)(void),
+                       uint32_t begin_time_ms,
+                       uint32_t repeat_time_ms )
+{
+}
+
+void schedulerStart( void )
+{
+}
+
+void schedulerDispatchTasks( void )
+{
+}
+
+void scheduleTasks( uint32_t ms)
+{
+}
+
